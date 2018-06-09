1913af5258f29da56c8d9505f85f03c83493b1ce
Nadeem133 <32464612+Nadeem133@users.noreply.github.com>
2018-06-09 10:37:21 -0400

    Part 1 - Person componet added

---
+++ src/app/person.ts
@@ -0,0 +1,19 @@
+export class Person {
+    id: number;
+    firstName: string;
+    lastName: string;
+    dateOfBirth: Date;
+
+    /* Add a method to this model called getAge() which will return an accurate age based on the
+dateOfBirth */
+    getAge(): number {
+        let today: Date = new Date();
+        let age: number = today.getFullYear() - this.dateOfBirth.getFullYear() ;
+
+        if(this.dateOfBirth.getDate() > today.getDate()
+        && this.dateOfBirth.getMonth() > today.getMonth()) {
+            age--;
+        }
+        return age;
+    }
+}
